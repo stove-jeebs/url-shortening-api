719d0d062261bc20a003d62593dbb3d7c5affa22
stove-jeebs <1876595827qq@gmail.com>
2022-06-11 12:41:41 +1200

    url error message

--- src/components/api/Api.jsx
+++ src/components/api/Api.jsx
@@ -67,35 +67,39 @@ export default function Api() {
     <div className="container my-40 -translate-y-20">
       <form
         onSubmit={onSubmit}
-        action="/test"
-        className="flex flex-col md:flex-row px-4 md:px-12 py-12 rounded-lg
-					bg-secondary bg-contain gap-6 md:gap-4 bg-cover bg-center"
+        action="/#"
+        className={`${
+          !isValid &&
+          "after:content-['Please_add_a_valid_link'] after:text-warning after:italic after:absolute"
+        } px-4 md:px-12 py-12 rounded-lg
+					bg-secondary bg-contain bg-cover bg-center`}
         style={{ backgroundImage: `url(${desktopPattern})` }}
       >
-        {/* link input field */}
-        <input
-          onChange={(e) => {
-            setText(e.target.value);
-            setIsValid(true);
-          }}
-          type="text"
-          className={`${
-            isValid
-              ? "border-transparent text-veryDarkViolet focus:border-primary"
-              : "border-warning text-warning focus:border-warning"
-          } rounded-xl px-4 py-3 text-xl placeholder:text-grayishViolet
+        <div className="flex flex-col md:flex-row w-full gap-6 md:gap-4">
+          {/* link input field */}
+          <input
+            onChange={(e) => {
+              setText(e.target.value);
+              setIsValid(true);
+            }}
+            type="text"
+            className={`${
+              isValid
+                ? "border-transparent text-veryDarkViolet focus:border-primary"
+                : "border-warning text-warning focus:border-warning"
+            } rounded-xl px-4 py-3 text-xl placeholder:text-grayishViolet
 						focus:outline-none border-[3px] flex-1`}
-          placeholder="Shorten a link here..."
-        />
-
-        {/* submit button */}
-        <button
-          type="submit"
-          className="bg-primary text-white hover:bg-primaryHover font-bold text-xl rounded-lg
+            placeholder="Shorten a link here..."
+          />
+          {/* submit button */}
+          <button
+            type="submit"
+            className="bg-primary text-white hover:bg-primaryHover font-bold text-xl rounded-lg
 						px-12 py-5"
-        >
-          Shorten!
-        </button>
+          >
+            Shorten!
+          </button>
+        </div>
       </form>
 
       {/* Api cards */}
