6e474af74b95740d30a279378a6b0e247b94af93
stove-jeebs <stove-jeebs@pop-os.localdomain>
2022-06-21 02:07:07 +1200

    stat icons

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,27 +1,10 @@
 import Card from "./InfoCards";
-import brandIcon from "../../assets/icon-brand-recognition.svg";
-import recordsIcon from "../../assets/icon-detailed-records.svg";
-import customizeIcon from "../../assets/icon-fully-customizable.svg";
+import data from "./InfoData";
 
 export default function Info() {
-  const data = {
-    brand: {
-      icon: { brandIcon },
-      title: "Brand Recognition",
-      text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
-    },
-    records: {
-      icon: { recordsIcon },
-      title: "Detailed Records",
-      text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
-    },
-    customize: {
-      icon: { customizeIcon },
-      title: "Fully Customizable",
-      text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
-    },
-  };
-
+  const cards = data.map((card) => {
+    return <Card key={data["key"]} {...card} />;
+  });
   return (
     <section>
       {/* title */}
@@ -35,11 +18,7 @@ export default function Info() {
         </p>
       </div>
       {/* information cards */}
-      <div className="container flex justify-center gap-6">
-        <Card {...data["brand"]} />
-        <Card {...data["records"]} />
-        <Card {...data["customize"]} />
-      </div>
+      <div className="container flex justify-center gap-6">{cards}</div>
     </section>
   );
 }
---
+++ src/components/info/InfoData.js
@@ -0,0 +1,24 @@
+const data = {
+  brand: {
+    key: "0",
+    icon: process.env.PUBLIC_URL + "/icons/icon-brand-recognition.svg",
+    title: "Brand Recognition",
+    text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+    position: "start",
+  },
+  records: {
+    key: "1",
+    icon: process.env.PUBLIC_URL + "/icons/icon-detailed-records.svg",
+    title: "Detailed Records",
+    text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+    position: "center",
+  },
+  customize: {
+    key: "2",
+    icon: process.env.PUBLIC_URL + "/icons/icon-fully-customizable.svg",
+    title: "Fully Customizable",
+    text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+    position: "end",
+  },
+};
+export default data;
